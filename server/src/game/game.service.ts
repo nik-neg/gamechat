c5e551c0d3e98b4d0f5d1100cdfa8f802877deae
francois <francoisadam@hotmail.be>
2021-05-11 14:55:46 +0200

    feat: add fetch game by name

--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateGamerDto } from 'src/gamer/dto/create-gamer.dto';
 import { Repository } from 'typeorm';
@@ -14,21 +18,60 @@ export class GameService {
     private readonly gameRepository: Repository<Game>,
   ) {}
 
-  async create(createGameDto: CreateGameDto, gameChatRoomId) {
+  async create(name, gameChatRoomId) {
     let game = new Game();
-    game = Object.assign(game, { ...createGameDto });
     game.gameChatRoom = gameChatRoomId;
-    // console.log(game);
-    try {
-      const response = await this.gameRepository.save(game);
-      console.log(response);
-      return response;
-    } catch (err) {
-      console.log(err);
-    }
+
+    const newGame = await this.fetchOne(name);
+    const gameSaved = await this.gameRepository.findOne({ apiId: newGame.id });
+    if (gameSaved) throw new ConflictException('Game already exists');
+
+    const screenshots = newGame.platforms
+      .map((p) => p.platform.image_background)
+      .concat(newGame.stores.map((s) => s.store.image_background));
+
+    const entity = {
+      apiId: newGame.id,
+      title: newGame.name_original || newGame.name,
+      genreList: newGame.genres.map(
+        ({ id, name }: { id: number; name: string }) => ({
+          id,
+          name,
+        }),
+      ),
+      dominantGenre: { id: newGame.genres[0].id, name: newGame.genres[0].name },
+      releaseDate: new Date(newGame.released).toISOString(),
+      imagesPath: { cover: newGame.background_image, screenshots },
+      consoles: newGame.platforms.map((p) => p.platform.name),
+      ageRating: newGame.esrb_rating ? newGame.esrb_rating.name : '',
+      description: newGame.description_raw,
+    };
+
+    game = Object.assign(game, { ...entity });
+
+    const response = await this.gameRepository.save(game);
+    return response;
+  }
+
+  async fetchOne(name: string) {
+    //TODO: some improvements are needed --> const newGame = res.data.results[0];
+    const res = await axios.get(
+      `${process.env.GAME_API_URL}/games?key=${process.env.GAME_API_KEY}&search=${name}`,
+    );
+
+    const newGame = res.data.results[0];
+    if (!newGame) throw new NotFoundException('The game does not exist');
+
+    const resDetails = await axios.get(
+      `${process.env.GAME_API_URL}/games/${newGame.id}?key=${process.env.GAME_API_KEY}`,
+    );
+
+    const { platforms, stores, description_raw } = resDetails.data;
+    return { ...newGame, platforms, stores, description_raw };
   }
 
   async uploadOne(apiId: number) {
+    //Uploads game by apiId to database
     const game = await this.gameRepository.findOne({ apiId });
     if (game) return game;
     const res = await axios.get(
@@ -41,7 +84,6 @@ export class GameService {
       ({ image }: { image: string }) => image,
     );
 
-    console.log(res);
     const newGame = res.data;
 
     if (!newGame) throw new NotFoundException('The newGame does not exist');
