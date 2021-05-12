f943466804ca82f56a6863630486de071fa1e0f0
francois <francoisadam@hotmail.be>
2021-05-12 12:02:38 +0200

    build: add sass configuration

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <header className={classes['App-header']}>
         <p>
           {' '}
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes['App-link']}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
