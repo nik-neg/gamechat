63c2c6d7e298dc37da7ffd5eec083d9264504b71
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-16 21:38:31 +0200

    feat: setup home grid

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,8 +1,59 @@
 import React from 'react';
 import classes from './Home.module.scss';
 
-const Home = (): JSX.Element => {
-  return <div>Home</div>;
-};
+// const Home = (): JSX.Element => {
+//   return <div>Home</div>;
+// };
 
-export default Home;
+// export default Home;
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme: any) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function NestedGrid() {
+  const classes = useStyles();
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
