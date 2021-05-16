cbb7a642f1035402a41771ca0c188512ba1cbea4
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-16 22:38:15 +0200

    feat: add placeholder for nav bar

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -33,6 +33,19 @@ export default function NestedGrid() {
         justify="flex-start"
         alignItems="flex-start"
       >
+        <Grid item xs={12}>
+          <Grid item xs={12}>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </Grid>
+        </Grid>
         <Grid item xs={4}>
           <Grid
             container
