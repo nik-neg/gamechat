a8b215b85db48bfe2fc16a184997d7d94f3f5cac
Matt Srotyr <srotyr@gmx.com>
2021-05-19 13:58:36 +0100

    feat: profile page looking better

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import classes from './Profile.module.scss';
 import { Avatar } from '@material-ui/core';
 import { useAppSelector } from '../../hooks/redux';
+import { ReactComponent as ControllerPS1 } from '../../images/controller-playstation-01.svg';
+import { ReactComponent as ControllerPS2 } from '../../images/controller-playstation-02.svg';
+import { ReactComponent as ControllerPS3 } from '../../images/controller-playstation-03.svg';
+import { ReactComponent as ControllerXbox } from '../../images/controller-xbox.svg';
 
 //favourite Rooms
 
@@ -12,26 +16,42 @@ const Profile = (): JSX.Element => {
     return `${gamer.firstName} ${gamer.lastName}`.split(' ').map((n) => n[0]);
   };
 
+  function favGameString() {
+    if (!gamer.favouriteGameChats) return '';
+    let str = '';
+    gamer.favouriteGameChats.forEach((gc, index) => {
+      str += gc.id;
+      str += ', ';
+    });
+    return str.slice(0, -2);
+  }
+
   if (gamer) {
     return (
       <div className={classes.container}>
-        {/* <div className={classes.name}>
-          <Avatar
-            aria-label="recipe"
-            classes={{
-              colorDefault: classes['avatar-colorDefault'],
-            }}
-          >
-            {getInitial()}
-          </Avatar>
-          <h1>{gamer.firstName}</h1>
+        <ControllerPS3 className={classes.image} id={classes['ctr-ps-3']} />
+        <div className={classes.circle__container}>
+          <div className={classes.title__container}>
+            <h1>{gamer.firstName}</h1>
+            <Avatar
+              aria-label="recipe"
+              classes={{
+                root: classes.avatar,
+                colorDefault: classes['avatar-colorDefault'],
+              }}
+            >
+              {getInitial()}
+            </Avatar>
+          </div>
+          <div className={classes.information}>
+            <div className={classes.details}>Details:</div>
+            <pre> {`First Name: ${gamer.firstName}`}</pre>
+            <pre> {`Last Name: ${gamer.lastName}`}</pre>
+            <pre> {`Email: ${gamer.email}`}</pre>
+            <pre> {`Language: ${gamer.language}`}</pre>
+            <pre> {`Favourite Game Chats: ${favGameString()}`}</pre>
+          </div>
         </div>
-        <h2>Favourite Game Chats:</h2>
-        {gamer.favouriteGameChats
-          ? gamer.favouriteGameChats.map((gameChat) => {
-              return <span key={gameChat.id}>{gameChat.id}</span>;
-            })
-          : null} */}
       </div>
     );
   } else return <div>Hello</div>;
